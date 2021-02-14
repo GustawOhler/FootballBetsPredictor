b145ec798d265d5d8935a592c13c0f3b2d25ce29
Gucio <gustaw@ohler.pl>
2021-02-15 00:08:19 +0100

    Ulepszenie sieci neuronowej

--- neural_network_manager.py
+++ neural_network_manager.py
@@ -7,6 +7,9 @@ from dataset_creator import split_dataset
 import numpy as np
 
 
+results_to_description_dict = {0: 'Wygrana gospodarzy', 1: 'Remis', 2: 'Wygrana gości'}
+
+
 def plot_metric(history, metric):
     train_metrics = history.history[metric]
     val_metrics = history.history['val_' + metric]
@@ -32,9 +35,7 @@ def show_winnings(predicted_classes, actual_classes, odds):
 
 def show_accuracy_for_classes(predicted_classes, actual_classes):
     predicted_classes_as_int = predicted_classes
-        # np.argmax(predicted_classes, axis=1)
     actual_classes_as_int = actual_classes
-        # np.argmax(actual_classes, axis=1)
     comparison_array = actual_classes_as_int == predicted_classes_as_int
     for i in np.unique(actual_classes_as_int):
         current_actual_class_indexes = [index for index, class_value in enumerate(actual_classes_as_int) if class_value == i]
@@ -43,48 +44,54 @@ def show_accuracy_for_classes(predicted_classes, actual_classes):
         false_positives = sum(1 for comparison in comparison_array[current_predicted_class_indexes] if not comparison)
         all_actual_class_examples = len(current_actual_class_indexes)
         all_predicted_class_examples = len(current_predicted_class_indexes)
-        print("Skutecznosc dla klasy " + str(i) + " = " + str(true_positives/all_actual_class_examples) + "% (" +
-              str(true_positives) + "/" + str(all_actual_class_examples) + ")")
-        print("Ilosc falszywie przewidzianych dla klasy " + str(i) + " = " + str(false_positives / all_predicted_class_examples)
+        print("Procent odgadniętych przykładów na wszystkie przykłady z klasą \"" + results_to_description_dict[i]
+              + "\" = {:.1f}".format(100*true_positives/all_actual_class_examples if all_actual_class_examples != 0 else 0)
+              + "% (" + str(true_positives) + "/" + str(all_actual_class_examples) + ")")
+        print("Ilosc falszywie przewidzianych dla klasy \"" + results_to_description_dict[i]
+              + "\" = {:.1f}".format(100*false_positives / all_predicted_class_examples if all_predicted_class_examples != 0 else 0)
               + "% (" + str(false_positives) + "/" + str(all_predicted_class_examples) + ")")
 
 
 def perform_nn_learning(dataset):
     x, y, odds = split_dataset(dataset)
-    factor = 1e-5
+    factor = 0.0001
     rate = 0.1
 
     # layer = keras.Normalization()
     # layer.adapt(x)
     model_input = keras.Input(shape=(x.shape[1],))
     # model = layer(model_input)
-    model = keras.layers.Dense(512, activation='relu', kernel_regularizer=l2(factor))(model_input)
+    model = keras.layers.BatchNormalization()(model_input)
+    model = keras.layers.Dense(4096, activation='relu', activity_regularizer=l2(factor))(model)
+    model = keras.layers.BatchNormalization()(model)
     model = keras.layers.Dropout(rate)(model)
-    model = keras.layers.Dense(256, activation='relu', kernel_regularizer=l2(factor))(model)
+    model = keras.layers.Dense(2048, activation='relu', activity_regularizer=l2(factor))(model)
+    model = keras.layers.BatchNormalization()(model)
     model = keras.layers.Dropout(rate)(model)
-    # model = keras.layers.Dense(256, activation='relu', kernel_regularizer=l2(factor))(model)
-    # model = keras.layers.Dropout(rate)(model)
-    model = keras.layers.Dense(128, activation='relu', kernel_regularizer=l2(factor))(model)
+    model = keras.layers.Dense(1024, activation='relu', activity_regularizer=l2(factor))(model)
+    model = keras.layers.BatchNormalization()(model)
     model = keras.layers.Dropout(rate)(model)
-    model = keras.layers.Dense(64, activation='relu', kernel_regularizer=l2(factor))(model)
+    model = keras.layers.Dense(512, activation='relu', activity_regularizer=l2(factor))(model)
+    model = keras.layers.BatchNormalization()(model)
     model = keras.layers.Dropout(rate)(model)
-    # model = keras.layers.Dense(64, activation='relu', kernel_regularizer=l2(factor))(model)
-    # model = keras.layers.Dropout(rate)(model)
-    model = keras.layers.Dense(32, activation='relu', kernel_regularizer=l2(factor))(model)
+    model = keras.layers.Dense(256, activation='relu', activity_regularizer=l2(factor))(model)
+    model = keras.layers.BatchNormalization()(model)
     model = keras.layers.Dropout(rate)(model)
-    model = keras.layers.Dense(16, activation='relu', kernel_regularizer=l2(factor))(model)
-    model = keras.layers.Dropout(rate)(model)
+    model = keras.layers.Dense(64, activation='relu', activity_regularizer=l2(factor))(model)
+    model = keras.layers.BatchNormalization()(model)
+    model = keras.layers.Dropout(rate/4)(model)
+    model = keras.layers.Dense(16, activation='relu', activity_regularizer=l2(factor))(model)
     output = keras.layers.Dense(3, activation='softmax')(model)
     model = keras.Model(inputs=model_input, outputs=output)
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    history = model.fit(x, y, epochs=100, batch_size=64, verbose=2, shuffle=True, validation_split=0.15)
+    history = model.fit(x, y, epochs=35, batch_size=128, verbose=1, shuffle=True, validation_split=0.15)
 
     y_prob = model.predict(x)
     y_classes = y_prob.argmax(axis=-1)
     show_winnings(y_classes, y.argmax(axis=-1), odds)
     show_accuracy_for_classes(y_classes, y.argmax(axis=-1))
 
-    plot_metric(history, 'accuracy')
+    plot_metric(history, 'loss')
