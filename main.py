1c2ae1e6a405c9a487462c3b498e5efbe450bee4
Gucio <gustaw@ohler.pl>
2021-03-20 20:40:17 +0100

    Automated downloading matches data from football-data website

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from csv_processor import process_csv_and_save_to_db
 from database_helper import setup_db
 from dataset_creator import create_dataset, load_dataset
 from neural_network_manager import perform_nn_learning
+import web_data_scraper
 
 NEED_TO_DROP_TABLES = False
 SHOULD_LOG = False
 NEED_TO_CREATE_DATASET = False
 
-setup_db(SHOULD_LOG, NEED_TO_DROP_TABLES)
-csv_folder_path = '.\\MatchesData\\UsedData'
-only_files_in_dir = [join(csv_folder_path, f) for f in listdir(csv_folder_path) if isfile(join(csv_folder_path, f))]
-for file in only_files_in_dir:
-    print(file)
-    process_csv_and_save_to_db(file)
-
-if NEED_TO_CREATE_DATASET:
-    dataset = create_dataset()
-else:
-    dataset = load_dataset()
-
-perform_nn_learning(dataset)
+# setup_db(SHOULD_LOG, NEED_TO_DROP_TABLES)
+# csv_folder_path = '.\\MatchesData\\UsedData'
+# only_files_in_dir = [join(csv_folder_path, f) for f in listdir(csv_folder_path) if isfile(join(csv_folder_path, f))]
+# for file in only_files_in_dir:
+#     print(file)
+#     process_csv_and_save_to_db(file)
+#
+# if NEED_TO_CREATE_DATASET:
+#     dataset = create_dataset()
+# else:
+#     dataset = load_dataset()
+#
+# perform_nn_learning(dataset)
+web_data_scraper.download_data_from_web()
